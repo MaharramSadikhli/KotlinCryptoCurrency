ac6b9f5ce92e57bbf25b50c2b4463a65a0f8063b
Muarem <s.muarem10@gmail.com>
2024-02-14 05:08:22 +0400

    Coroutines used instead of RxJava

--- app/src/main/java/com/imsoft/kotlincryptocurrency/service/CryptoCurrencyAPI.kt
+++ app/src/main/java/com/imsoft/kotlincryptocurrency/service/CryptoCurrencyAPI.kt
@@ -3,6 +3,7 @@ package com.imsoft.kotlincryptocurrency.service
 import com.imsoft.kotlincryptocurrency.model.CryptoCurrencyModel
 import io.reactivex.Observable
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CryptoCurrencyAPI {
@@ -11,9 +12,9 @@ interface CryptoCurrencyAPI {
     // MaharramSadikhli/APIs/main/cryptocurrencyapi.json
 
     @GET("MaharramSadikhli/APIs/main/cryptocurrencyapi.json")
-    fun getData(): Observable<List<CryptoCurrencyModel>>
+    suspend fun getData(): Response<List<CryptoCurrencyModel>>
 
-
+    //fun getData(): Observable<List<CryptoCurrencyModel>>
     //fun getData(): Call<List<CryptoCurrencyModel>>
 
 }
